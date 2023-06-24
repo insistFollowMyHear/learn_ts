e6f1512ed3937dc1bb3e023507476ed7531ca619
Dong <17521231934@163.com>
2023-06-24 14:03:19 +0800

    Object

--- 3 - myObject/build/js/myObject.js
+++ 3 - myObject/build/js/myObject.js
@@ -1,33 +1,13 @@
 "use strict";
-// 规定形参接收的类型
-function signUpUser(name, email, isPaid = false) { }
-// 只能返回数字
-function addTwo(num) {
-    return num + 2;
-}
-// 只能返回布尔值
-function myValue(val) {
-    if (val > 5) {
-        return true;
-    }
-    return false;
-}
-// 箭头函数，规定只能返回字符串
-const getHello = (s) => {
-    return '';
+const User = {
+    name: 'hero',
+    email: 'hero@gamil.com',
+    isActive: false
 };
-// 数组循环规定每个元素都是字符串 / 数字，返回值只能是字符串
-const heros = ['thor', 'spiderman', 'ironman', 1];
-heros.map((hero) => {
-    console.log(hero);
-    return `hero is ${hero}`;
-});
-function consoleError(errmsg) {
-    console.log(errmsg);
+function createUser(obj) {
+    console.log(obj);
 }
-function handleError(errmsg) {
-    throw new Error(errmsg);
-}
-function heroName(hero, sex) {
-    console.log(hero + sex);
+createUser({ name: 'hero', isPaid: false });
+function createCourse() {
+    return { name: 'hero', price: 99.89 };
 }
--- 3 - myObject/src/myObject.ts
+++ 3 - myObject/src/myObject.ts
@@ -1,39 +1,21 @@
-// 规定形参接收的类型
-function signUpUser(name: string, email: string, isPaid: boolean = false) {}
-
-// 只能返回数字
-function addTwo(num: number): number {
-   return num + 2
+interface StaicUser {
+  name: string,
+  email: string,
+  isActive?: boolean,
+  [propName: string]: any // 定义了任意属性，属性 key 类型为字符串
 }
 
-// 只能返回布尔值
-function myValue(val: number): boolean {
-  if (val > 5) {
-    return true
-  }
-  return false
+const User: StaicUser = {
+  name: 'hero',
+  email: 'hero@gamil.com',
+  isActive: false
 }
 
-// 箭头函数，规定只能返回字符串
-const getHello = (s: string):string => {
-  return ''
+function createUser(obj: {name: string, isPaid: boolean}){ // 接收的对象必须有明确 类型定义
+  console.log(obj)
 }
+createUser({ name: 'hero', isPaid: false})
 
-// 数组循环规定每个元素都是字符串 / 数字，返回值只能是字符串
-const heros = ['thor', 'spiderman', 'ironman', 1]
-heros.map((hero: string | number): string => {
-  console.log(hero);
-  return `hero is ${hero}`
-})
-
-function consoleError(errmsg: string): void {
-  console.log(errmsg)
-}
-
-function handleError(errmsg: string): never {
-  throw new Error(errmsg)
-}
-
-function heroName(hero: string, sex?: string): void {
-  console.log(hero + sex)
+function createCourse():{name: string, price: number} {
+  return {name: 'hero', price: 99.89}
 }
